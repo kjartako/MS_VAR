65d767da0d7d6f13a0e153c41e94803ae148e154
Kjartan Kloster Osmundsen <kjartan.osmundsen@gmail.com>
2019-03-26 17:00:00 +0100

    Add files via upload

---
+++ MS_VAR/MS_VAR_example_USmacro.R
@@ -0,0 +1,50 @@
+library(coda)
+library(rstan)
+options(mc.cores = parallel::detectCores())
+
+chains=8
+it_burnin = 500
+it_sample = 1000
+
+sm = stan_model('Stan/MS_VAR_wishart.stan',auto_write=TRUE)
+#sm = stan_model('Stan/MS_VAR_lkj.stan',auto_write=TRUE)
+
+######################################################################################
+
+Y = unname(as.matrix(read.table("Data/USmacro.txt",sep="\t")))
+n=nrow(Y)
+
+######################################################################################
+
+n_dim=ncol(Y)
+n_AR=4
+n_reg=2
+reg_m=FALSE
+reg_s=TRUE
+
+stanfit = sampling(sm,data=list(T=n,dim=n_dim,y=Y,ARdim=n_AR,nreg=n_reg,mean_reg=reg_m,sigma_reg=reg_s,Q_alpha=as.array(rep(1,n_reg)),eye=diag(n_dim),
+                   L=chol(diag(n_dim)),nu=n_dim+1),iter=(it_burnin+it_sample), warmup = it_burnin, chains=chains,
+                   pars=c("Q","mu","phi","sigma","S"))
+
+# stanfit = sampling(sm,data=list(T=n,dim=n_dim,y=Y,ARdim=n_AR,nreg=n_reg,mean_reg=reg_m,sigma_reg=reg_s,Q_alpha=as.array(rep(1,n_reg)),
+#                    eta=1,gamma_alpha=2,gamma_beta=0.1),iter=(it_burnin+it_sample), warmup = it_burnin, chains=chains,
+#                    pars=c("Q","mu","phi","sigma","S"))
+
+######################################################################################
+# Label-switching
+
+pars =mcmc.list(lapply(1:ncol(stanfit), function(x) mcmc(as.array(stanfit)[,x,])))
+
+source("labsw_2reg.R")
+pars_ls = labsw_2reg(pars,chains,n_dim,n_AR,n_reg,reg_m,reg_s)
+
+######################################################################################
+# Results
+
+options(scipen=999)
+summary(pars_ls$pars)$statistics[1:pars_ls$index[1],]
+plot(pars_ls$pars[,c(1,2,pars_ls$index[2],pars_ls$index[2]+1)])
+
+states=as.matrix(pars_ls$pars)[,(pars_ls$index[1]+1):pars_ls$index[3]]
+plot(colMeans(states),ylab="Mean state")
+plot(round(colMeans(states),0),ylab="Most likely state")
---
+++ MS_VAR/labsw_2reg.R
@@ -0,0 +1,48 @@
+labsw_2reg = function(pars, chains,n_dim,n_AR,n_reg,reg_m,reg_s)
+{
+  # Reorder Q[2,1] and Q[2,2], to make permutation easier
+  for(i in 1:chains)
+  {
+    pars[[i]][,c(2,4)]=pars[[i]][,c(4,2)]
+    dimnames(pars[[i]])$parameters[c(2,4)]=dimnames(pars[[i]])$parameters[c(4,2)]
+  }
+  # Identify parameters indexes
+  n_m= max(1,reg_m*n_reg)
+  n_s= max(1,reg_s*n_reg)
+  
+  q_range = c(1:n_reg^2)
+  mean_range = c(1:((n_m*n_dim)+(n_m*n_AR*n_dim^2)))+max(q_range)
+  sigma_range= c(1:(n_s*n_dim^2))+max(mean_range) 
+  
+  # Identify which parameters to permutate
+  permu_range = q_range
+  if(reg_m){permu_range=c(permu_range,mean_range)}
+  if(reg_s){permu_range=c(permu_range,sigma_range)}
+  
+  index_lastpar=max(sigma_range)
+  index_laststate=index_lastpar+n-n_AR
+  
+  # Impose identifiability constraint, sigma[1,1,1]<sigma[2,1,1]
+  index_s=index_lastpar-(n_s*n_dim^2)+1
+  for(i in 1:chains)
+  {
+    chainvals=pars[[i]]
+    
+    if(chainvals[1,index_s]>chainvals[1,index_s+1])
+    {
+      # permute parameters
+      for(j in permu_range[seq(from=1,to=length(permu_range),by=2)])
+      {
+        pars[[i]][,c(j,j+1)]=chainvals[,c(j+1,j)]
+      }
+      # permute states
+      tmp=pars[[i]][,(index_lastpar+1):index_laststate]
+      tmp[(pars[[i]][,(index_lastpar+1):index_laststate])==1]=2
+      tmp[(pars[[i]][,(index_lastpar+1):index_laststate])==2]=1
+      
+      pars[[i]][,(index_lastpar+1):index_laststate]=tmp
+    }
+  }
+  
+  return(list(pars=pars,index=c(index_lastpar,index_s,index_laststate)))
+}
